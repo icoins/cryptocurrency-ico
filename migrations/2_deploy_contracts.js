3fc388bfabc561a1fa832b11bf459a970a35d411
Papan Das <hexoindia@gmail.com>
2018-06-14 20:10:32 +0530

    Smart Contract Ready and Deployed on Test Network

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var PTokenSales = artifacts.require("./PTokenSales.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(PTokenSales);
+  deployer.deploy(PTokenSales, 100000, "PaCoin", "PaCo", 18, "0x309Ac76512679A9E82e2Cf8Cb5987E2FD5A0e5c0");
 };
