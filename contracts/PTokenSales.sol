83e7228fc7dad85e2f860a1f49c0fd25c7b84166
Papan Das <hexoindia@gmail.com>
2018-06-14 19:19:21 +0530

    initial testing

--- contracts/PTokenSales.sol
+++ contracts/PTokenSales.sol
@@ -2,9 +2,11 @@ pragma solidity ^0.4.17;
 
 contract PTokenSales {
     address public owner;
+    string public ownname;
     
-    constructor () public {
+    function PTokenSales () public {
         owner = msg.sender;
+        ownname = "Papan Das";
     }
   
 }
